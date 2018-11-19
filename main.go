4af8ea37f11addded1eebfa508e71e8d1d5ff860
Miles Maddox <miles.maddox@edointeractive.com>
2018-11-19 17:08:52 -0600

    test structure

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(CloudWatchEventsHandler)
+}
+
+// CloudWatchEventsHandler handles executions from CloudWatch Events scheduler
+func CloudWatchEventsHandler(e events.CloudWatchEvent) (s string, err error) {
+	outputJSON, _ := json.Marshal(e)
+	// stdout and stderr are sent to AWS CloudWatch Logs
+	fmt.Println(string(outputJSON[:]))
+
+	return
 }
