390c6a4849553fb4fb7f140238ed10fa52272a05
Miles Maddox <miles.maddox@edointeractive.com>
2018-11-20 16:04:41 -0600

    export yesterday's logs

--- main.go
+++ main.go
@@ -1,22 +1,53 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"time"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
+	"github.com/jinzhu/now"
 )
 
-func main() {
-	lambda.Start(CloudWatchEventsHandler)
+var (
+	defaultDescription = "desired version set by lambda ecs-deploy"
+	sess               = session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+)
+
+func main() { lambda.Start(ExportLogs) }
+
+// BucketExportConfigs provides options for the export
+type BucketExportConfigs struct {
+	S3Bucket string `json:"s3_bucket"`
+	S3Prefix string `json:"s3_prefix"`
+	LogGroup string `json:"log_group"`
 }
 
-// CloudWatchEventsHandler handles executions from CloudWatch Events scheduler
-func CloudWatchEventsHandler(e events.CloudWatchEvent) (s string, err error) {
-	outputJSON, _ := json.Marshal(e)
-	// stdout and stderr are sent to AWS CloudWatch Logs
-	fmt.Println(string(outputJSON[:]))
+// ExportLogs handles executions from CloudWatch Events scheduler
+func ExportLogs(conf BucketExportConfigs) (s string, err error) {
+	fmt.Printf("Exporting log group %s to s3://%s/%s\n", conf.LogGroup, conf.S3Bucket, conf.S3Prefix)
+	yesterday := time.Now().AddDate(0, 0, -1)
 
+	fmt.Println(conf.S3Prefix + "/" + yesterday.Format("year=2006/month=01/day=02"))
+	client := cloudwatchlogs.New(sess)
+	params := &cloudwatchlogs.CreateExportTaskInput{
+		Destination:       aws.String(conf.S3Bucket),
+		DestinationPrefix: aws.String(conf.S3Prefix + "/" + yesterday.Format("year=2006/month=01/day=02")),
+		From:              aws.Int64(now.New(yesterday).BeginningOfDay().Unix() * 1000),
+		LogGroupName:      aws.String(conf.LogGroup),
+		TaskName:          aws.String("exporter"),
+		To:                aws.Int64(now.New(yesterday).EndOfDay().Unix() * 1000),
+	}
+	res, err := client.CreateExportTask(params)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if res.TaskId != nil {
+		fmt.Printf("Started export task %s\n", *res.TaskId)
+	}
 	return
 }
